c3a35ceb570304124b4b8965d57ae66c5c980ae1
Keno Maerz <keno.maerz@googlemail.com>
2019-03-30 19:12:10 +0100

    Added an exception for unresolvable access keys

---
+++ pymcws/exceptions.py
@@ -0,0 +1,16 @@
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class UnresolvableKeyError(Error):
+    """Exception raised if access key is unresolvable.
+
+    Attributes:
+        key -- The key that could not be resolved
+        message -- message as given by jriver web service
+    """
+
+    def __init__(self, key, message):
+        self.key = key
+        self.message = message
