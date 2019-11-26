df9f142de54bba073f1377c3de720b1feaff3859
Keno Maerz <k.maerz@dkfz-heidelberg.de>
2019-11-26 13:37:12 +0100

    Black code

--- pymcws/query_recipes.py
+++ pymcws/query_recipes.py
@@ -8,21 +8,30 @@ def query_album(media_server: MediaServer, album_artist: str, album: str) -> Dic
     """
     album_artist = escape_for_query(album_artist)
     album = escape_for_query(album)
-    query = '[Album Artist]=[' + album_artist + '] [Album]=[' + album + '] ~sort=[Disc #],[Track #]'
-    response = files_search(media_server, query, 'MPL')
+    query = (
+        "[Album Artist]=["
+        + album_artist
+        + "] [Album]=["
+        + album
+        + "] ~sort=[Disc #],[Track #]"
+    )
+    response = files_search(media_server, query, "MPL")
     return response
 
-def query_keyword(media_server: MediaServer, keyword: str, sort_criteria: List[str] = None) -> Dict:
+
+def query_keyword(
+    media_server: MediaServer, keyword: str, sort_criteria: List[str] = None
+) -> Dict:
     """Returns all files tagged with a specific keyword.
 
     sort_criteria: a list of strings that contain fieldnames by which to sort the result
     """
     keyword = escape_for_query(keyword)
-    query = '[keywords]=[' + keyword + ']'
+    query = "[keywords]=[" + keyword + "]"
     if sort_criteria is not None:
         query += " ~sort="
         for criterion in sort_criteria:
             query += "[" + criterion + "],"
         query = query[:-1]
-    response = files_search(media_server, query, 'MPL')
+    response = files_search(media_server, query, "MPL")
     return response
