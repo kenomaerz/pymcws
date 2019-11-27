a4a7fc3ae60b41282a9ade84e083d8544b041a5f
Keno Maerz <keno.maerz@googlemail.com>
2019-11-27 09:50:38 +0100

    Forward zones in play recipes
    Zones previously were ignored when using play recipes

--- pymcws/play_recipes.py
+++ pymcws/play_recipes.py
@@ -5,18 +5,18 @@ from .api import files_search, escape_for_query, playback_repeat, playback_shuff
 def play_album(media_server: MediaServer, album_artist: str, album: str,
                shuffle: bool = False, use_play_doctor: bool = False,
                repeat: bool = None, zone: Zone = None):
-    """ Plays an Album by a given Album Artist.
+    """ Plays an album by a given album artist.
     
         By default, this call disables shuffle, so the album can be listened in the intended order.
         set shuffle to None to avoid this and preserve shuffle state.
-        Setting shuffle to True only shuffles order of files and leaves playback state alone
+        Setting shuffle to True only shuffles order of files and leaves playback state alone.
     """
     album_artist = escape_for_query(album_artist)
     album = escape_for_query(album)
     if shuffle is False:
         playback_shuffle(media_server, mode='Off')
     query = '[Album Artist]=[' + album_artist + '] [Album]=[' + album + '] ~sort=[Disc #],[Track #]'
-    response = files_search(media_server, query, 'play', shuffle=shuffle, use_play_doctor=use_play_doctor)
+    response = files_search(media_server, query, 'play', shuffle=shuffle, use_play_doctor=use_play_doctor, zone=zone)
     response.raise_for_status()
     if repeat is not None:
         repeat = 'Playlist' if repeat else 'Off'
@@ -28,5 +28,5 @@ def play_keyword(media_server: MediaServer, keyword: str, use_play_doctor: bool 
     """
     keyword = escape_for_query(keyword)
     query = '[keywords]=[' + keyword + ']'
-    response = files_search(media_server, query, 'play',  use_play_doctor=use_play_doctor, shuffle=shuffle)
+    response = files_search(media_server, query, 'play',  use_play_doctor=use_play_doctor, shuffle=shuffle, zone=zone)
     response.raise_for_status()
